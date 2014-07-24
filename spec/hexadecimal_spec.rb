7a1e6c0e3d636410569279834390ab0be8387d19
Justin Speers & mark rubenstein <student@epicodus.com>
2014-07-24 14:08:42 -0700

    create hexadecimal function to convert hex to decimal numbers

--- spec/hexadecimal_spec.rb
+++ spec/hexadecimal_spec.rb
@@ -6,5 +6,23 @@ describe :hexadecimal do
   it 'returns 10 for A' do
     expect(hexadecimal("A")).to eq(10)
   end
+
+  it 'returns 15 for F' do
+    expect(hexadecimal('F')).to eq(15)
+  end
+
+  it 'returns 16 for 10' do
+    expect(hexadecimal('10')).to eq(16)
+  end
+
+  it 'returns 255 for FF' do
+    expect(hexadecimal('ff')).to eq(255)
+  end
+
+  it 'returns 123456789 for 75BCD15' do
+    expect(hexadecimal('75BCD15')).to eq(123456789)
+  end
+
 end
+
 
